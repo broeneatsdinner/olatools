567c7d2f2a960edec442144c8e1e6d9ba0107559
broeneatsdinner <9823270+broeneatsdinner@users.noreply.github.com>
2025-01-27 15:40:29 -0700

    Add setup.sh for quick run

---
+++ dotfiles/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# --------------------
+# --    setup.sh    --
+# --------------------
+
+# Sets up the dotfiles for use in the current environment
+source $HOME/olatools/dotfiles/.bash_aliases
+source $HOME/olatools/dotfiles/.bash_functions
+source $HOME/olatools/dotfiles/.bash_profile
+source $HOME/olatools/dotfiles/.best_bash_history
